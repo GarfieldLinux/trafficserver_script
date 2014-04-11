8eb056ec53dbff46c66f65a479fc5534453e547f
Brian Geffon <briang@apache.org>
2014-04-11 10:00:30 -0700

    formatting fixes

--- ts_script.cpp
+++ ts_script.cpp
@@ -10,26 +10,26 @@
 extern "C" int yyparse(ASTNode **);
 
 extern "C" void yyerror(ASTNode **node, const char *s, ...) {
-	va_list ap;
-	va_start(ap, s);
+  va_list ap;
+  va_start(ap, s);
 
-	fprintf(stderr, "line %d: ", yylineno);
-	vfprintf(stderr, s, ap);
-	fprintf(stderr, "\n");
+  fprintf(stderr, "line %d: ", yylineno);
+  vfprintf(stderr, s, ap);
+  fprintf(stderr, "\n");
 }
 
 int main(int argc, char **argv) {
-	ASTNode *root = NULL;
-	int ret_val = yyparse(&root);
-	fprintf(stdout, "-----------------\n");
-	std::ofstream outputFile("parse_tree.dot");
-	root->printDot(outputFile);
-	outputFile.close();
+  ASTNode *root = NULL;
+  int ret_val = yyparse(&root);
+  fprintf(stdout, "-----------------\n");
+  std::ofstream outputFile("parse_tree.dot");
+  root->printDot(outputFile);
+  outputFile.close();
 
-	std::ofstream outputCode("plugin.c");
-	CCompiledTarget target(outputCode);
-	target.generate(root);
-	outputCode.close();
+  std::ofstream outputCode("plugin.c");
+  CCompiledTarget target(outputCode);
+  target.generate(root);
+  outputCode.close();
 
-	return 0;
+  return 0;
 }
